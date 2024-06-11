5ac641d12daf30c93b22237eb3ea6ff7644a6081
VtrHype <77414160+VtrHype@users.noreply.github.com>
2024-06-11 06:42:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// scipt.js
 document.addEventListener('DOMContentLoaded', () => {
+    // Função para obter a data atual
     function getHoraAtual() {
         const now = new Date();
         const dia = String(now.getDate()).padStart(2, '0');
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('data').value = getHoraAtual();
 
+    // Função para finalizar a ronda
     async function finalizarRonda(event) {
         if (event.target.classList.contains('finalizar-ronda')) {
             const row = event.target.closest('tr');
@@ -37,7 +38,19 @@ document.addEventListener('DOMContentLoaded', () => {
             const motivoDisparo = row.querySelector('.motivo_disparo').value;
             const descricao = row.querySelector('.descricao').value;
 
-            console.log({ nome, matricula, placa, cliente, hChegada, hSaidaValue, contatoLocal, motivoDisparo, descricao, kmInicio, kmFinal, });
+            console.log({
+                nome,
+                matricula,
+                placa,
+                cliente,
+                hChegada,
+                hSaidaValue,
+                contatoLocal,
+                motivoDisparo,
+                descricao,
+                kmInicio,
+                kmFinal,
+            });
 
             const rondaData = [{
                 nome,
@@ -54,7 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }];
 
             try {
-                const response = await fetch('https://4e10-2804-58bc-1072-7300-bc79-4d03-9874-ee3f.ngrok-free.app/api/rondas/save', {
+                const response = await fetch('http://localhost:3000/api/rondas/save', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
@@ -94,7 +107,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         <label for="contato_local">Contato no Local</label>
                         <input type="text" class="contato_local" id="contato_local" style="text-transform: uppercase;">
                     </td>
-                    <<td>
+                    <td>
                     <label for="motivo_disparo">Motivo</label>
                     <select class="motivo_disparo" id="motivo_disparo" style="text-transform: uppercase;">
                     <option>Selecione</option>
@@ -126,7 +139,6 @@ document.addEventListener('DOMContentLoaded', () => {
                         <input type="time" class="h_saida" id="h_saida" style="text-transform: uppercase; display: none;">
                         <button class="btn finalizar-ronda">Finalizar Ronda</button>
                     </td>
-
         `;
 
         document.getElementById('in').appendChild(newRow);
@@ -148,6 +160,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         newRow.querySelector('.finalizar-ronda').addEventListener('click', () => {
             addRowButton.disabled = false;
+            saveToLocalStorage(); // Salvar estado ao finalizar ronda
         });
     });
 
@@ -168,6 +181,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.querySelector('.finalizar-ronda').addEventListener('click', () => {
         addRowButton.disabled = false;
+        saveToLocalStorage(); // Salvar estado ao finalizar ronda
     });
 
     // Função para limpar os campos da planilha
@@ -180,68 +194,175 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('data').value = getHoraAtual();
         document.querySelectorAll('#in tr').forEach(row => row.remove());
         addRowButton.disabled = false;
+        localStorage.removeItem('formState'); // Limpar localStorage ao finalizar a ronda
     }
+
+    // Função para salvar o estado atual no localStorage
+    function saveToLocalStorage() {
+        const formState = {
+            nome: document.getElementById('nome').value,
+            matricula: document.getElementById('matricula').value,
+            placa: document.getElementById('placa').value,
+            kmInicio: document.getElementById('kmInicio').value,
+            kmFinal: document.getElementById('kmFinal').value,
+            data: document.getElementById('data').value,
+            rows: Array.from(document.querySelectorAll('#in tr')).map(row => ({
+                cliente: row.querySelector('.cliente').value,
+                hChegada: row.querySelector('.h_chegada').value,
+                hSaida: row.querySelector('.h_saida').value,
+                contatoLocal: row.querySelector('.contato_local').value,
+                motivoDisparo: row.querySelector('.motivo_disparo').value,
+                descricao: row.querySelector('.descricao').value
+            }))
+        };
+        localStorage.setItem('formState', JSON.stringify(formState));
+    }
+
+    // Função para carregar o estado do localStorage
+    function loadFromLocalStorage() {
+        const formState = JSON.parse(localStorage.getItem('formState'));
+        if (formState) {
+            document.getElementById('nome').value = formState.nome;
+            document.getElementById('matricula').value = formState.matricula;
+            document.getElementById('placa').value = formState.placa;
+            document.getElementById('kmInicio').value = formState.kmInicio;
+            document.getElementById('kmFinal').value = formState.kmFinal;
+            document.getElementById('data').value = formState.data;
+            formState.rows.forEach(rowData => {
+                const newRow = document.createElement('tr');
+                newRow.classList.add('minha_classe');
+
+                newRow.innerHTML = `
+                <hr class="linha"> 
+                <td>
+                                <label for="cliente">Cliente</label>
+                                <input type="text" class="cliente" value="${rowData.cliente}" style="text-transform: uppercase;">
+                            </td>
+                            
+                            <td>
+                                <label for="contato_local">Contato no Local</label>
+                                <input type="text" class="contato_local" value="${rowData.contatoLocal}" style="text-transform: uppercase;">
+                            </td>
+                            <td>
+                            <label for="motivo_disparo">Motivo</label>
+                            <select class="motivo_disparo" value="${rowData.motivoDisparo}" style="text-transform: uppercase;">
+                            <option>Selecione</option>
+                            <option>Ronda</option>
+                            <option>Ronda Interna</option>
+                            <option>Entrega de Equipamentos</option>
+                            <option>Alarme</option>
+                            <option>Atendimento Solicitado</option>
+                            <option>Pânico</option>
+                            <option>PB</option>
+                            <option>QAP</option>
+                            <option>Rendição QRF</option>
+                            <option>Trocar de Moto</option>
+                            <option>Manutenção Moto</option>
+                            <option>Alteração no Local</option>
+                            </select>
+                        </td>
+                            <td>
+                                <label for="descricao">Descrição</label>
+                                <input type="text" class="descricao" value="${rowData.descricao}" style="text-transform: uppercase;">
+                            </td>
+                            
+                            <td>
+                                <label for="h_chegada">Horário de Chegada</label>
+                                <input type="time" class="h_chegada" value="${rowData.hChegada}" style="text-transform: uppercase; display: block;">
+                            </td>
+                            <td class="btn-container">
+                                <label for="h_saida">Horário de Saída</label>
+                                <input type="time" class="h_saida" value="${rowData.hSaida}" style="text-transform: uppercase; display: ${rowData.hSaida ? 'inline' : 'none'};">
+                                <button class="btn finalizar-ronda">Finalizar Ronda</button>
+                            </td>
+                `;
+
+                document.getElementById('in').appendChild(newRow);
+
+                const clienteInput = newRow.querySelector('.cliente');
+                const hChegadaInput = newRow.querySelector('.h_chegada');
+
+                clienteInput.addEventListener('input', () => {
+                    if (clienteInput.value.trim() === '') {
+                        hChegadaInput.style.display = 'none';
+                    } else {
+                        const now = new Date();
+                        hChegadaInput.value = now.toTimeString().split(' ')[0];
+                        hChegadaInput.style.display = 'block';
+                    }
+                });
+
+                addRowButton.disabled = true;
+
+                newRow.querySelector('.finalizar-ronda').addEventListener('click', () => {
+                    addRowButton.disabled = false;
+                    saveToLocalStorage(); // Salvar estado ao finalizar ronda
+                });
+            });
+        }
+    }
+
+    loadFromLocalStorage(); // Carregar o estado ao abrir o aplicativo
 
     // Função para gerar o PDF
-document.getElementById('finalizar-turno').addEventListener('click', async () => {
-    if (confirm('Deseja finalizar o Relatório?')) {
-        const { jsPDF } = window.jspdf;
-        const doc = new jsPDF();
+    document.getElementById('finalizar-turno').addEventListener('click', async () => {
+        if (confirm('Deseja finalizar o Relatório?')) {
+            const { jsPDF } = window.jspdf;
+            const doc = new jsPDF();
 
-        const nome = document.getElementById('nome').value;
-        const matricula = document.getElementById('matricula').value;
-        const placa = document.getElementById('placa').value;
-        const data = document.getElementById('data').value;
-        const kmInicio = document.getElementById('kmInicio').value;
-        const kmFinal = document.getElementById('kmFinal').value;
+            const nome = document.getElementById('nome').value;
+            const matricula = document.getElementById('matricula').value;
+            const placa = document.getElementById('placa').value;
+            const data = document.getElementById('data').value;
+            const kmInicio = document.getElementById('kmInicio').value;
+            const kmFinal = document.getElementById('kmFinal').value;
 
-        doc.text('Relatório de Rondas', 14, 20);
-        doc.text(`Nome: ${nome}`, 14, 30);
-        doc.text(`Matrícula: ${matricula}`, 85, 30);
-        doc.text(`Placa: ${placa}`, 14, 40);
-        doc.text(`Data: ${data}`, 85, 20);
-        doc.text(`Km Início: ${kmInicio}`, 85, 40);
-        doc.text(`Km Final: ${kmFinal}`, 147, 40);
+            doc.text('Relatório de Rondas', 14, 20);
+            doc.text(`Nome: ${nome}`, 14, 30);
+            doc.text(`Matrícula: ${matricula}`, 85, 30);
+            doc.text(`Placa: ${placa}`, 14, 40);
+            doc.text(`Data: ${data}`, 85, 20);
+            doc.text(`Km Início: ${kmInicio}`, 85, 40);
+            doc.text(`Km Final: ${kmFinal}`, 147, 40);
 
-        const columns = ["Cliente", "Horário de Chegada", "Horário de Saída", "Contato no Local", " Motivo ", "Descrição"];
-        const rows = [];
+            const columns = ["Cliente", "Horário de Chegada", "Horário de Saída", "Contato no Local", " Motivo ", "Descrição"];
+            const rows = [];
 
-        document.querySelectorAll('#rondas_tabela tbody tr').forEach(row => {
-            const cliente = row.querySelector('.cliente').value;
-            const hChegada = row.querySelector('.h_chegada').value;
-            const hSaida = row.querySelector('.h_saida').value;
-            const contatoLocal = row.querySelector('.contato_local').value;
-            const motivoDisparo = row.querySelector('.motivo_disparo').value;
-            const descricao = row.querySelector('.descricao').value;
+            document.querySelectorAll('#rondas_tabela tbody tr').forEach(row => {
+                const cliente = row.querySelector('.cliente').value;
+                const hChegada = row.querySelector('.h_chegada').value;
+                const hSaida = row.querySelector('.h_saida').value;
+                const contatoLocal = row.querySelector('.contato_local').value;
+                const motivoDisparo = row.querySelector('.motivo_disparo').value;
+                const descricao = row.querySelector('.descricao').value;
 
-            rows.push([cliente, hChegada, hSaida, contatoLocal, motivoDisparo, descricao]);
-        });
+                rows.push([cliente, hChegada, hSaida, contatoLocal, motivoDisparo, descricao]);
+            });
 
-        doc.autoTable({
-            head: [columns],
-            body: rows,
-            startY: 50,
-            styles: { fontSize: 10, halign: 'center', valign: 'middle' },
-            headStyles: { fillColor: [0, 0, 0], textColor: [255, 255, 255] },
-            alternateRowStyles: { fillColor: [240, 240, 240] },
-        });
-        
+            doc.autoTable({
+                head: [columns],
+                body: rows,
+                startY: 50,
+                styles: { fontSize: 10, halign: 'center', valign: 'middle' },
+                headStyles: { fillColor: [0, 0, 0], textColor: [255, 255, 255] },
+                alternateRowStyles: { fillColor: [240, 240, 240] },
+            });
 
-        const nomeArquivo = `${nome.toUpperCase()}_${data.replace(/\//g, '-')}.pdf`;
+            const nomeArquivo = `${nome.toUpperCase()}_${data.replace(/\//g, '-')}.pdf`;
 
-        // Salvar PDF localmente
-        doc.save(nomeArquivo);
+            // Salvar PDF localmente
+            doc.save(nomeArquivo);
 
-        // Salvar PDF na interface Android
-        const pdfBase64 = doc.output('datauristring');
-        if (window.Android) {
-            window.Android.savePDF(nomeArquivo, pdfBase64);
-        } else {
-            console.error("Interface Android não encontrada!");
+            // Salvar PDF na interface Android
+            const pdfBase64 = doc.output('datauristring');
+            if (window.Android) {
+                window.Android.savePDF(nomeArquivo, pdfBase64);
+            } else {
+                console.error("Interface Android não encontrada!");
+            }
+
+            limparPlanilha();
         }
-
-        limparPlanilha();
-    }
-  });
+    });
 
 });
